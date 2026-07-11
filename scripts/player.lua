e6850a0b188b524d47146d283407815586a214f1
Tafari <tafarigriffith2016@gmail.com>
2026-07-10 20:16:22 -0600

    properly passes camera into player in player.lua

--- scripts/player.lua
+++ scripts/player.lua
@@ -6,7 +6,7 @@ function player:_ready()
 	local state_controller = require("StateController")
 	local PlayerStates = require("PlayerStates")
 	local player_camera = self:get_node("Camera")
-	state_controller.newPlayer(self, Camera, PlayerStates.Idle)
+	state_controller.newPlayer(self, player_camera, PlayerStates.Idle)
 end;
 
 function player:_process(dt)
